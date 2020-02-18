0193cd9ba4682b6415cfb4c71946304b48a115b5
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2020-02-18 23:11:44 +0800

    third commit

--- js/form.js
+++ js/form.js
@@ -1,34 +1,14 @@
-function submitEvent(){
-    var event = document.event_form.event;
-    var textOne = document.event_form.textOne;
-    var dateOne = document.event_form.dateOne;
-    var dateTwo = document.event_form.dateTwo;
-
-    if(event.value == "Choose one"){
-        alert("Event Type is required");
-        event.focus();
-        return false;
-    }
-    
-    if(dateOne.value.length <= 0){
-        alert("Event Date is required");
-        dateOne.focus();
-        return false;
-    }
-
-    if(dateTwo.value.length <= 0){
-        alert("Posting Date is required");
-        dateTwo.focus();
-        return false;
-    }
-
-    if(textOne.value.length <= 0){
-        alert("Event Title is required");
-        textOne.focus();
-        return false;
-    }
-    else{
-        alert("The form has been successfully submitted");
-    }
-return true;
+function submitPassword()
+{
+        var pass = document.getElementById('pass').value;
+        var confirmPassword = document.getElementById('confirmPassword').value;
+        
+        if (confirmPassword !== pass)
+        {
+                document.getElementById('confirmPassword').innerHTML = 'Passwords do not match';
+        }
+        else
+        {
+                document.getElementById('pass').innerHTML = 'Password Matched';
+        }
 }

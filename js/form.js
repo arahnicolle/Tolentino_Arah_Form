8e3d2b21cc0cbe9498dea50d63efe8503aa62ba6
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2020-02-19 20:17:01 +0800

    fourth commit

--- js/form.js
+++ js/form.js
@@ -1,14 +1,30 @@
 function submitPassword()
 {
-        var pass = document.getElementById('pass').value;
-        var confirmPassword = document.getElementById('confirmPassword').value;
+        let login = document.getElementById("login");
+        let first = document.getElementById("password1");
+        let checkpass = document.getElementById("password2");
+        let email = document.getElementById("email");
+
+        if (login.value != "") {
+                document.getElementById("login").style.backgroundColor = "#DFF2BF";
+            }
+            else if (login.value == "") {
+                document.getElementById("login").style.backgroundColor = "#FFD2D2";
+            }
         
-        if (confirmPassword !== pass)
-        {
-                document.getElementById('confirmPassword').innerHTML = 'Passwords do not match';
+            if (first == checkpass.value) {
+                document.getElementById("password").style.backgroundColor = "#FFD2D2";
+            }
+            else  {
+                    
+                document.getElementById("alert").innerHTML = "Passwords are not the same";
+                return true;
+            }
+            if (email.value == "") {
+                document.getElementById("email").style.backgroundColor = "#FFD2D2";
+            }
+            else if(email!=""){
+                document.getElementById("password").style.backgroundColor = "#DFF2BF";
+            }  
         }
-        else
-        {
-                document.getElementById('pass').innerHTML = 'Password Matched';
-        }
-}
+        submitPassword();
